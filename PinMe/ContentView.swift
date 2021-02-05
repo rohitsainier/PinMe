9e25bba108a4aa70a6c9aa7d5b78990f5f8e48dc
rohitsainier <rohitsainier@gmail.com>
2021-02-05 15:47:07 +0530

    Initial Commit

---
+++ PinMe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PinMe
+//
+//  Created by Rohit Saini on 05/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PinMe/PinMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinMeApp.swift
+//  PinMe
+//
+//  Created by Rohit Saini on 05/02/21.
+//
+
+import SwiftUI
+
+@main
+struct PinMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
